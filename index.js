442fb3d2197671c51c4571f075e2edeef7367a56
Pbknowall <pbknowall2@gmail.com>
2021-04-04 11:33:12 +0100

    Release

--- index.js
+++ index.js
@@ -15,19 +15,21 @@ client.on('ready', () => {
     let userCount = array.reduce((a, b) => a + b)
 
     console.log(`${client.user.tag} está online com ${userCount} utilizadores`)
-    client.user.setActivity(`mc.mangocraftpt.tk`)
+    client.user.setActivity(`on [NÃO LANÇADO]`)
 });
 
 client.on('guildMemberAdd', (member) => {
     const guildFetched = client.guilds.cache.get(server)
-    const welcomeChannel = guildFetched.channels.cache.get(process.env.JOINS)
-    const welcomeEmbed = new MessageEmbed()
-        .setAuthor(`Bem-vindo ${member.user.username}!`, guildFetched.iconURL())
-        .setDescription(`Dá uma vista de olhos em ${guildFetched.channels.cache.get('826119289664569405') ? guildFetched.channels.cache.get('826119289664569405') : '<#826119289664569405>'} e \n${guildFetched.channels.cache.get('826119312435445800') ? guildFetched.channels.cache.get('826119312435445800') : '<#826119312435445800>'} para começar!`)
-        .setColor('RANDOM')
-        .setThumbnail(member.user.avatarURL())
-        .setTimestamp()
-    welcomeChannel.send(welcomeEmbed)
+    if (!member.user.bot) {
+        const welcomeChannel = guildFetched.channels.cache.get(process.env.JOINS)
+        const welcomeEmbed = new MessageEmbed()
+            .setAuthor(`Bem-vindo ${member.user.username}!`, guildFetched.iconURL())
+            .setDescription(`Dá uma vista de olhos em ${guildFetched.channels.cache.get('826119289664569405') ? guildFetched.channels.cache.get('826119289664569405') : '<#826119289664569405>'} e \n${guildFetched.channels.cache.get('826119312435445800') ? guildFetched.channels.cache.get('826119312435445800') : '<#826119312435445800>'} para começar!`)
+            .setColor('RANDOM')
+            .setThumbnail(member.user.avatarURL())
+            .setTimestamp()
+        welcomeChannel.send(welcomeEmbed)
+    }
 
     const joinRole = guildFetched.roles.cache.get('826126149536055377') || guildFetched.roles.cache.find(role => role.name === 'Membro')
     member.roles.add(joinRole)
@@ -61,22 +63,37 @@ client.on('message', async (message) => {
                 message.channel.send(`De momento estou a \`${client.ws.ping}ms\` de ping!`)
                 break;
 
+
             case 'eval':
                 if (owners.includes(message.author.id)) {
                     try {
                         const res = await eval(args.slice(1).join(" "))
                         message.channel.send(`\`\`\`js\n${res}\`\`\``)
                     } catch (err) {
-                        message.channel.send('Error', `\`\`\`js\n${err}\`\`\``)
+                        message.channel.send(['Error', `\`\`\`js\n${err}\`\`\``])
                     }
                 }
                 break;
-                
-                case 'anuncie':
-                    const items = args.slice(2).join(' ').split('|')
-                    const anuncio = args[1] === '-a' ? '826119350543974420' : '826122694466469941'
-                    message.guild.channels.cache.get(anuncio).send('<@283312969931292672>', { embed: { title: items[0], description: items[1], timestamp: Date.now(), footer: { text: `Anúncio por ${message.author.tag}`, iconURL: message.guild.iconURL() }, color: '#fa820a' } }).then(() => message.react('✅')).then(r => { r.remove() }, 3000)
-                    break;
+
+
+            case 'anuncie':
+                if (message.author.hasPermission('GUILD')) break;
+                const items = args.slice(2).join(' ').split('|')
+                const anuncio = args[1] === '-a' ? '826119350543974420' : '826122694466469941'
+                message.guild.channels.cache.get(anuncio).send('<@283312969931292672>', {
+                    embed: {
+                        title: items[0] ? items[0] : 'Título não fornecido',
+                        description: items[1] ? items[1] : 'Corpo não fornecido',
+                        timestamp: Date.now(),
+                        footer: {
+                            text: `${args[1] === '-a' ? 'Anúncio' : 'Atualização'} por ${message.author.tag}`,
+                            iconURL: message.guild.iconURL()
+                        },
+                        color: '#fa820a'
+                    }
+                })
+                    .then(() => message.react('✅'))
+                break;
         }
     } else {
         if (message.channel.id === '826124252494430308') {
